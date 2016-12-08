a97ed291a60e7ebc53ade3c66af3aed06b38b264
malin patel <malin.patel@icloud.com>
2016-12-08 11:47:32 +0000

    Check energy usage added & refactored code

--- jasmine-standalone-2.5.2/spec/thermostatSpec.js
+++ jasmine-standalone-2.5.2/spec/thermostatSpec.js
@@ -8,21 +8,17 @@ describe('Thermostat', function() {
   });
 
   it('should start at 20 degrees', function() {
-    expect(thermostat.temperature()).toEqual(20);
+    expect(thermostat.currentTemperature()).toEqual(20);
   });
 
   it('should increase the temperature', function() {
     thermostat.up();
-    expect(thermostat.temperature()).toEqual(21);
-  });
-
-  it('should have a down function', function() {
-    expect('down' in thermostat).toEqual(true);
+    expect(thermostat.currentTemperature()).toEqual(21);
   });
 
   it('should decrease the temperature', function() {
     thermostat.down();
-    expect(thermostat.temperature()).toEqual(19);
+    expect(thermostat.currentTemperature()).toEqual(19);
   });
 
   it('has a minimum temperature of 10 degrees', function() {
@@ -34,7 +30,7 @@ describe('Thermostat', function() {
   });
 
   it('has a default maximum temperature of 25 degrees', function() {
-    expect(thermostat._maxTemperature).toEqual(25);
+    expect(thermostat.MAX_LIMIT_PSM_ON).toEqual(25);
   });
 
   it('power saving mode can be turned off', function() {
@@ -44,12 +40,30 @@ describe('Thermostat', function() {
 
   it('has a maximum temperature of 32 degrees when power saving mode is off', function() {
     thermostat.turnOffPowerSaving();
-    expect(thermostat._maxTemperature).toEqual(32);
+    expect(thermostat.MAX_LIMIT_PSM_OFF).toEqual(32);
   });
 
   it('temperature can be reset with a reset function',function(){
     thermostat.up();
     thermostat.reset();
     expect(thermostat._temperature).toEqual(20);
+  });
+
+  it('checks for high energy usage',function(){
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.checkEnergyUsage()).toEqual('high-usage');
+  });
+
+  it('checks for low energy usage',function(){
+    for (var i = 0; i < 3; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.checkEnergyUsage()).toEqual('low-usage');
+  });
+
+  it('checks for medium energy usage',function(){
+    expect(thermostat.checkEnergyUsage()).toEqual('medium-usage');
   });
 });
--- jasmine-standalone-2.5.2/src/thermostat.js
+++ jasmine-standalone-2.5.2/src/thermostat.js
@@ -1,27 +1,49 @@
 function Thermostat(){
   this._temperature = 20;
   this._powerSavingMode = true;
-  this._maxTemperature = 25;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
   this.MIN_TEMPERATURE = 10;
+  this.DEFAULT_TEMP = 20;
+  this.LOW_ENERGY_USAGE_LIMIT = 18
+  this.HIGH_ENERGY_USAGE_LIMIT = 25
 }
 
-Thermostat.prototype.temperature = function() {
+Thermostat.prototype.currentTemperature = function() {
   return this._temperature;
 };
 
 Thermostat.prototype.up = function() {
-  this._temperature = this._temperature + 1;
+  if (this.powerSavingMode === true && this._temperature === this.MAX_LIMIT_PSM_ON) {
+    return;
+  } else if (this.powerSavingMode === false && this._temperature === this.MAX_LIMIT_PSM_OFF) {
+    return;
+  } else {
+    this._temperature += 1;
+  };
 };
 
 Thermostat.prototype.down = function() {
-  this._temperature = this._temperature - 1;
+  if (this._temperature === this.MIN_TEMPERATURE) {
+    return;
+  }
+    this._temperature -= 1;
 };
 
 Thermostat.prototype.turnOffPowerSaving = function() {
   this._powerSavingMode = false;
-  this._maxTemperature = 32;
 };
 
 Thermostat.prototype.reset = function() {
-  this._temperature = 20;
+  this._temperature = this.DEFAULT_TEMP;
+};
+
+Thermostat.prototype.checkEnergyUsage = function() {
+  if (this._temperature < this.LOW_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  } else if (this._temperature >= this.HIGH_ENERGY_USAGE_LIMIT) {
+    return 'high-usage';
+  } else {
+    return 'medium-usage';
+  }
 };
