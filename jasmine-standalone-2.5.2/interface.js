60a31e2de4b1a21873ebc9ade116ef524aa050fd
malin patel <malin.patel@icloud.com>
2016-12-08 15:25:27 +0000

    Added updateTemperature to interface.js

--- jasmine-standalone-2.5.2/interface.js
+++ jasmine-standalone-2.5.2/interface.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat._temperature);
+  updateTemperature();
+
     $("#up").click(function() {
       thermostat.up();
+      updateTemperature();
+      
     });
     $("#down").click(function() {
       thermostat.down();
+      updateTemperature();
     });
     $("#reset").click(function() {
       thermostat.reset();
+      updateTemperature();
     });
     $("#PSM-off").click(function() {
       thermostat.turnOffPowerSaving();
+      updateTemperature();
     });
     $("#PSM-on").click(function() {
       thermostat.turnOnPowerSaving();
+      updateTemperature();
     });
+
+    function updateTemperature() {
+      $('#temperature').text(thermostat._temperature);
+    };
 });
