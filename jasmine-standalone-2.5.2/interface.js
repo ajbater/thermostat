a44a5aae516fdfbd6977e2e76901c5b9b9f19434
ajbater <amanda22@hotmail.co.uk>
2016-12-08 16:54:42 +0000

    using weather api

--- jasmine-standalone-2.5.2/interface.js
+++ jasmine-standalone-2.5.2/interface.js
@@ -24,6 +24,16 @@ $(document).ready(function() {
       updateTemperature();
     });
 
+    $('#current-city').change(function() {
+      var city = $('#current-city').val();
+      var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+      var token = '&appid=e5cee232ac717bdd15541a4aceb02aa2';
+      var units = '&units=metric';
+      $.get(url + token + units, function(data) {
+        $('#current-temperature').text(data.main.temp);
+      })
+    })
+
     function updateTemperature() {
       $('#temperature').text(thermostat._temperature);
       $('#temperature').attr('class', thermostat.checkEnergyUsage());
